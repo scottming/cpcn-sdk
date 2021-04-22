df8d2fe1a52e403fd704d2a9b2b490e310ff4ff3
scottming <therealscottming@gmail.com>
2021-04-22 11:44:29 +0800

    Modify some mix config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule CPCNSdk.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/scottming/cpcn-sdk"
+
   def project do
     [
       app: :cpcn_sdk,
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "A sdk for CPCN payment.",
+      deps: deps(),
+      package: [
+        files: ~w(mix.exs lib README.md),
+        licenses: ["MIT"],
+        maintainers: ["ScottMing"],
+        links: %{
+          "GitHub" => @github_url
+        }
+      ]
     ]
   end
 
@@ -25,7 +36,8 @@ defmodule CPCNSdk.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
 
       {:xml_builder, "~> 2.1"},
-      {:exsync, "~> 0.2", only: :dev}
+      {:exsync, "~> 0.2", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
 
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
